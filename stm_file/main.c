0d8d60d088aa0fbee02e61718f78dec2fd231c95
Krystian Józefiak <k.jozefiak@curulis.pl>
2016-04-10 17:21:48 +0200

    Keyboard controls
    Accelerometer and  left, right, down, up arrows

--- stm_file/main.c
+++ stm_file/main.c
@@ -1,20 +1,25 @@
-/**
- *	Keil project for USB HID Device
- *
- *	Before you start, select your target, on the right of the "Load" button
- *
- *	@author 	Tilen Majerle
- *	@email		tilen@majerle.eu
- *	@website	http://stm32f4-discovery.com
- *	@ide		Keil uVision 5
- */
 /* Include core modules */
 #include "stm32f4xx.h"
 /* Include my libraries here */
+
 #include "defines.h"
 #include "tm_stm32f4_usb_hid_device.h"
 #include "tm_stm32f4_delay.h"
 #include "tm_stm32f4_disco.h"
+#include "stm32f4_discovery_lis302dl.h"
+
+int acc_x, acc_y, acc_z;
+
+void initAccelerometer()
+{
+	LIS302DL_InitTypeDef LIS302DL_InitStruct;
+	LIS302DL_InitStruct.Power_Mode=LIS302DL_LOWPOWERMODE_ACTIVE;
+	LIS302DL_InitStruct.Output_DataRate=LIS302DL_DATARATE_100;
+	LIS302DL_InitStruct.Axes_Enable=LIS302DL_XYZ_ENABLE;
+	LIS302DL_InitStruct.Full_Scale=LIS302DL_FULLSCALE_2_3;
+	LIS302DL_InitStruct.Self_Test = LIS302DL_SELFTEST_NORMAL;
+	LIS302DL_Init(&LIS302DL_InitStruct);
+}
 
 int main(void) {
 	uint8_t already = 0;
@@ -47,41 +52,37 @@ int main(void) {
 	TM_USB_HIDDEVICE_GamepadStructInit(&Gamepad1);
 	TM_USB_HIDDEVICE_GamepadStructInit(&Gamepad2);
 
+	initAccelerometer();
+
 	while (1) {
 		TM_DISCO_LedOn(LED_ALL);
 		/* If we are connected and drivers are OK */
+
 		if (TM_USB_HIDDEVICE_GetStatus() == TM_USB_HIDDEVICE_Status_Connected) {
 			/* Turn on green LED */
 			TM_DISCO_LedOn(LED_RED);
 
-/* Simple sketch start */
+			LIS302DL_Read(&acc_x,LIS302DL_OUT_X_ADDR,1);
+			LIS302DL_Read(&acc_y,LIS302DL_OUT_Y_ADDR,1);
+			LIS302DL_Read(&acc_z,LIS302DL_OUT_Z_ADDR,1);
 
-			/* If you pressed button right now and was not already pressed */
-			if (TM_DISCO_ButtonPressed() && already == 0) { /* Button on press */
-				already = 1;
-
-				/* Set pressed keys = WIN + R */
-				Keyboard.L_GUI = TM_USB_HIDDEVICE_Button_Pressed;	/* Win button */
-				Keyboard.Key1 = 0x15; 								/* R */
-				/* Result = "Run" command */
-
-				/* Send keyboard report */
+			if(acc_y < 240 && acc_y > 190){
+				Keyboard.Key1 = 0x51; /* DOWN ARROW */
 				TM_USB_HIDDEVICE_KeyboardSend(&Keyboard);
-			} else if (!TM_DISCO_ButtonPressed() && already == 1) { /* Button on release */
-				already = 0;
-
-				/* Release all buttons */
-				Keyboard.L_GUI = TM_USB_HIDDEVICE_Button_Released;	/* No button */
-				Keyboard.Key1 = 0x00; 								/* No key */
-				/* Result = Released everything */
-
-				/* Send keyboard report */
+			}else if(acc_y > 20 && acc_y < 60){
+				Keyboard.Key1 = 0x52; /* UP ARROW */
+				TM_USB_HIDDEVICE_KeyboardSend(&Keyboard);
+			}else if(acc_x > 210 && acc_x < 245){
+				Keyboard.Key1 = 0x50; /* LEFT ARROW */
+				TM_USB_HIDDEVICE_KeyboardSend(&Keyboard);
+			}else if(acc_x > 10 && acc_x < 60){
+				Keyboard.Key1 = 0x4F; /* RIGHT ARROW */
+				TM_USB_HIDDEVICE_KeyboardSend(&Keyboard);
+			}else{
+				Keyboard.Key1 = 0x00; /* No key */
 				TM_USB_HIDDEVICE_KeyboardSend(&Keyboard);
 			}
-
-/* Simple sketch end */
-
-		} else {
+		}else {
 			/* Turn off green LED */
 			TM_DISCO_LedOff(LED_GREEN);
 		}

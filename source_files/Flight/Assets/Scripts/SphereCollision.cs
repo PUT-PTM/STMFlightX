b9d1289adeb870983540dc66aef985458e136356
fj2016 <kfigiel.projekty@gmail.com>
2016-05-17 12:56:09 +0200

    New explosion effect during baloon colision
    When the plane hit the baloon, it explode.

--- source_files/Flight/Assets/Scripts/SphereCollision.cs
+++ source_files/Flight/Assets/Scripts/SphereCollision.cs
@@ -1,14 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class SphereCollision : MonoBehaviour {
+
+    private string lastName = String.Empty; 
 
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.name.Contains("Sphere"))
         {
-            Destroy(col.gameObject);
-            ScoreCount.score += 10;
+            if (lastName != col.gameObject.name || lastName == String.Empty)
+            {
+                lastName = col.gameObject.name;
+                ScoreCount.score += 10;
+                var tmp = col.gameObject.transform.position;
+                var sph = GameObject.Instantiate(GameObject.Find("Detonator-Insanity"), new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);
+                Destroy(col.gameObject);
+            }
         }
     }
 }

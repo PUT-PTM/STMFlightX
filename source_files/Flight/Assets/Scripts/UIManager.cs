10d7d333b5fc8b599c1fdf81bb2c2a8631e2b1b8
fj2016 <kfigiel.projekty@gmail.com>
2016-05-30 16:03:42 +0200

    Pause Menu - when someone click 'P' button

---
+++ source_files/Flight/Assets/Scripts/UIManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+    GameObject[] pauseObjects;
+
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = 1; //pause not loaded
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        hidePaused();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                showPaused();
+            }
+            else if (Time.timeScale == 0)
+            {
+                Debug.Log("high");
+                Time.timeScale = 1;
+                hidePaused();
+            }
+        }
+	}
+
+    public void Reload()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void pauseControl()
+    {
+        if(Time.timeScale==1)
+        {
+            Time.timeScale = 0;
+            showPaused();
+        }
+        else if (Time.timeScale==0)
+        {
+            Time.timeScale = 1;
+            hidePaused();
+        }
+    }
+
+    public void showPaused()
+    {
+        foreach(GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    public void hidePaused()
+    {
+        foreach(GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    public void LoadLevel (string level)
+    {
+        Application.LoadLevel(level);
+    }
+}
